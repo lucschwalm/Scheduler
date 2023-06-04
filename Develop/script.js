6d5d5ac9fcdd317d24fa6fa8efc1897b7170b129
lucschwalm <lucschwalm03@gmail.com>
2023-06-04 17:48:19 -0500

    Implemented interactivity with javascript

--- Develop/script.js
+++ Develop/script.js
@@ -42,9 +42,16 @@ $(function () {
       $(currentBlock).addClass("future");
     }
   }
-  // TODO: Add code to get any user input that was saved in localStorage and set
+  // Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  for(let i=0; i<timeBlocks.length; i++) {
+    var currentBlock = timeBlocks[i];
+    var storedInput = localStorage.getItem($(currentBlock).attr("id"));
+    $(currentBlock).children(".description").val(storedInput);
+  }
+  // Add code to display the current date in the header of the page.
+  var currentDate = dayjs().format("DD/MM/YYYY");
+  var timeEl = $("#currentDay");
+  timeEl.text("Today's date: " + currentDate);
 });
